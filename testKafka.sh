ffc42170e60b20a3535ecaaa1ccf6ce5d6786118
krudisar <krudisar@gmail.com>
2021-08-19 12:45:21 +0200

    Update testKafka.sh

--- testKafka.sh
+++ testKafka.sh
@@ -7,13 +7,16 @@ cd kafka_2.13-2.8.0/bin/
 bash kafka-topics.sh --create --topic quickstart-events --bootstrap-server localhost:9092
 bash kafka-topics.sh --create --topic quickstart-events-1 --bootstrap-server kafka1.krdemo.local:9092
 bash kafka-topics.sh --create --topic quickstart-events-2 --bootstrap-server kafka2.krdemo.local:9092
+bash kafka-topics.sh --create --topic quickstart-events-3 --bootstrap-server kafka3.krdemo.local:9092
 
 # ... get topic description referencing the 'other' broker
-bash kafka-topics.sh --describe --topic quickstart-events-2 --bootstrap-server kafka1.krdemo.local:9092
+bash kafka-topics.sh --describe --topic quickstart-events-3 --bootstrap-server kafka1.krdemo.local:9092
 bash kafka-topics.sh --describe --topic quickstart-events-1 --bootstrap-server kafka2.krdemo.local:9092
+bash kafka-topics.sh --describe --topic quickstart-events-2 --bootstrap-server kafka3.krdemo.local:9092
 
 # ... list all availble topics
 bash kafka-topics.sh --list --zookeeper localhost:2181
 bash kafka-topics.sh --list --zookeeper kafka1.krdemo.local:2181
 bash kafka-topics.sh --list --zookeeper kafka2.krdemo.local:2181
+bash kafka-topics.sh --list --zookeeper kafka3.krdemo.local:2181
 # ------- End of testing ...
